ad65ef8e7deb14eb66a69d12582858abd9be5260
Danilo Patanè <d.patane98@gmail.com>
2020-08-04 16:12:26 +0200

    aggiunta di commenti

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
+// Chiedo il nome del passeggero
 var nomePasseggero = prompt("Come ti chiami?")
-
+// Chiedo i Km da percorrere
 var chilometri = parseInt(prompt("Inserisci il numero di chilometri da percorrere"));
+// Chiedo l'età
 var etaPasseggero = parseInt(prompt("Quanti hanni hai?"));
+// indico una variabile con lo sconto minorenne
 var scontoMinorenne = parseInt((chilometri *0.21) *20 / 100);
+// indico una variabile con lo sconto terza eta
 var scontoTerzaEta = parseInt((chilometri *0.21) *60 / 100);
+// nella if calcolo il totale del prezzo
 if (isNaN(etaPasseggero)) {
   alert("Mi sa che devi metterci un numero")
 } else if (etaPasseggero <= 17) {
@@ -16,4 +21,5 @@ if (isNaN(etaPasseggero)) {
   var totaleNoSconto = chilometri * 0.21;
   document.getElementById('prezzo').innerHTML = totaleNoSconto;
 }
+// Stampo il nome sulla pagina
 document.getElementById('prezzo').innerHTML = prezzoPasseggero;
