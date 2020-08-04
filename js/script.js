047b32353a62938c8a1855499d5aace14c721301
Danilo Patanè <d.patane98@gmail.com>
2020-08-04 15:33:41 +0200

    End

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
+var nomePasseggero = prompt("Come ti chiami?")
+
 var chilometri = parseInt(prompt("Inserisci il numero di chilometri da percorrere"));
 var etaPasseggero = parseInt(prompt("Quanti hanni hai?"));
 var scontoMinorenne = parseInt((chilometri *0.21) *20 / 100);
-
+var scontoTerzaEta = parseInt((chilometri *0.21) *60 / 100);
 if (isNaN(etaPasseggero)) {
   alert("Mi sa che devi metterci un numero")
-} else if (etaPasseggero < 18) {
+} else if (etaPasseggero <= 17) {
   var totaleMinorenneConSconto = (chilometri * 0.21) - scontoMinorenne;
-  console.log(totaleMinorenneConSconto);
+  document.getElementById('prezzo').innerHTML = totaleMinorenneConSconto;
+} else if (etaPasseggero >= 65) {
+  var totaleTerzaEtaConSconto = (chilometri*0.21) - scontoTerzaEta;
+  document.getElementById('prezzo').innerHTML = totaleTerzaEtaConSconto;
+} else {
+  var totaleNoSconto = chilometri * 0.21;
+  document.getElementById('prezzo').innerHTML = totaleNoSconto;
 }
+document.getElementById('prezzo').innerHTML = prezzoPasseggero;
